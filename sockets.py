b25946ac3ec61206cdaa29bd674c6067e6933869
Matthew Mullen <matthew.mullenx@gmail.com>
2021-11-28 21:44:11 -0700

    finished

--- sockets.py
+++ sockets.py
@@ -15,17 +15,20 @@
 #
 import flask
 from flask import Flask, request
+from flask import redirect
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
 import time
 import json
 import os
-
+# https://www.gevent.org/api/gevent.queue.html
+# https://www.gevent.org/api/gevent.html
+# https://www.gevent.org/api/gevent.greenlet.html#gevent.Greenlet
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
-
+clientList=[]
 class World:
     def __init__(self):
         self.clear()
@@ -60,28 +63,63 @@ class World:
         return self.space
 
 myWorld = World()        
-
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    update = {entity:data}
+    for client in clientList:
+        client.put_nowait(json.dumps(update))
+
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect('/static/index.html')
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            update = ws.receive()
+            if update:
+                entities = json.loads(update)
+                for entity in entities:
+                    myWorld.set(entity,entities[entity])
+            else:
+                break
+    except:
+        pass
+
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    # this mean we have new client
+    # to represent new client add a gevent.queue to clientList
+    # then instantiate world for client
+    """spawn(function, *args, **kwargs) → Greenlet
+Create a new Greenlet object and schedule it to run function(*args, **kwargs). This can be used as gevent.spawn or Greenlet.spawn.
+
+The arguments are passed to Greenlet.__init__()."""
+    client:queue.Queue = queue.Queue()
+    clientList.append(client)
+    greenlet = gevent.spawn(read_ws,ws,client)
+    client.put_nowait(json.dumps(myWorld.world()))
+    try:
+        while True:
+            print("update sent if this goes thru")
+            update = client.get()
+            ws.send(update)
+            print("it went thru")
+    except:
+        pass
+    clientList.remove(client)
+    gevent.kill(greenlet)
+
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +137,28 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    updates:dict = flask_post_json()
+    for update in updates.keys():
+        myWorld.update(entity,update,updates[updates])
+    return flask.jsonify(myWorld.world()) if request.method!="PUT" else flask.jsonify(updates)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return flask.jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    #return flask.jsonify(entity)
+    return flask.jsonify(myWorld.get( entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return flask.jsonify(myWorld.world())
 
 
 
